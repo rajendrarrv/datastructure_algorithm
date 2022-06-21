3e727dd7ab7ecd69e9352c6af4184dfb6155fea6
rajendratechment <rajendra.verma@techment.com>
2022-06-21 20:39:48 +0530

    Assignment Coding Problem Problem Name: Sum of even & odd Problem Level: MEDIUM Problem Description: #### Write a program to input an integer N and print the sum of all its even digits and sum of all its odd digits separately. Digits mean numbers, not the places! That is, if the given integer is "13245",  even digits are 2 & 4 and odd digits are 1, 3 & 5.
         Integer N
    
        Sum_of_Even_Digits Sum_of_Odd_Digits
        (Print first even sum and then odd sum separated by space)
    
        0 <= N <= 10^8
     #####Sample Input 1:
        1234
    
        6 4
    
        552245
    
        8 15
    
        For the given input, the even digits are 2, 2 and 4 and if we take the sum of these digits it will come out to be 8(2 + 2 + 4) and similarly, if we look at the odd digits, they are, 5, 5 and 5 which makes a sum of 15(5 + 5 + 5). Hence the answer would be, 8(evenSum) <single space> 15(oddSum)

--- src/big/o/notation/Driver.java
+++ src/big/o/notation/Driver.java
@@ -1,10 +1,12 @@
 package big.o.notation;
 
+import ninja.SumOfEvenAndOdd;
 import ninja.TotalSalary;
 
 public class Driver {
     public static void main(String[] args) {
-        System.out.print("Net Salary " + new TotalSalary().netSalary(4567, 'B'));
+//        System.out.print("Net Salary " + new TotalSalary().netSalary(4567, 'B'));
+        new SumOfEvenAndOdd().sum(12345);
     }
 
 }
