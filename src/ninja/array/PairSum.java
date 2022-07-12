9a0a5f030fa1d69d43f4678bac6336265492bf14
rajendrarrv <rajendrarajaram@hotmail.com>
2022-07-12 18:58:48 +0530

    Pair sum https://trello.com/c/A3Qbi6yI/79-pairsum

--- src/ninja/array/PairSum.java
+++ src/ninja/array/PairSum.java
@@ -1,4 +1,23 @@
-package ninja.array;/**
-* Created by Rajendra Verma on 05/07/22.
-*/public class PairSum {
+package ninja.array;
+
+/**
+ * Created by Rajendra Verma on 05/07/22.
+ */
+public class PairSum {
+    public int solution(int array[], int x) {
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                int a = array[i];
+                int b = array[j];
+
+                int sum = a + b;
+
+                if (sum == x) count++;
+
+            }
+        }
+        return count;
+    }
 }
