4b404dc421dcb9942e77295bda8b4ed94bfea96b
rajendrarrv <rajendrarajaram@hotmail.com>
2022-07-12 19:01:23 +0530

    sotring bubble sort https://trello.com/c/dXhSrwC3/99-sorting

--- src/ninja/array/Sorting.java
+++ src/ninja/array/Sorting.java
@@ -1,4 +1,39 @@
-package ninja.array;/**
-* Created by Rajendra Verma on 10/07/22.
-*/public class Sorting {
+package ninja.array;
+
+/**
+ * Created by Rajendra Verma on 10/07/22.
+ */
+public class Sorting {
+    public void solution(int array[]) {
+
+        for (int k = 0; k < array.length; k++) {
+            System.out.print(" "+array[k]);
+        }
+        System.out.println();
+        System.out.println("---before--");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+
+                int a = array[i];
+                int b = array[j];
+
+                if (a>b) {
+                    int temp = a;
+                    a = b;
+                    b = temp;
+                    array[i] = a;
+                    array[j] = b;
+                }
+
+            }
+
+        }
+        System.out.println("--after---");
+
+        for (int k = 0; k < array.length; k++) {
+            System.out.print(" "+array[k]);
+        }
+
+
+    }
 }
