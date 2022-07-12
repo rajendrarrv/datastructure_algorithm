16bd7374d2a21f944fede227de8d49191c04f46a
rajendrarrv <rajendrarajaram@hotmail.com>
2022-07-12 19:00:23 +0530

    Triple sum https://trello.com/c/YOBBUXKU/80-triplet-sum

--- src/ninja/array/TripleSum.java
+++ src/ninja/array/TripleSum.java
@@ -4,4 +4,22 @@ package ninja.array;
  * Created by Rajendra Verma on 09/07/22.
  */
 public class TripleSum {
+    public int solution(int[] array, int x) {
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                for (int k = j + 1; k < array.length; k++) {
+                    int a = array[i];
+                    int b = array[j];
+                    int c = array[k];
+                    int sum = a + b + c;
+                    if (sum == x) counter++;
+
+                }
+            }
+        }
+
+        return counter;
+    }
+
 }
